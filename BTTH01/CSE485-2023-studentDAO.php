e642aae7e90ebb1ef19f1e296df5461f2aefc2e8
nguyennhuvinh <2051063937@e.tlu.edu.vn>
2023-05-09 09:11:59 +0700

    them tao vs read

--- BTTH01/CSE485-2023-studentDAO.php
+++ BTTH01/CSE485-2023-studentDAO.php
@@ -2,6 +2,19 @@
 class studentDAO extends Student{
     private $students = array();
     
+    public function create($student) {
+        array_push($this->students, $student);
+      }
+    
+      // phương thức lấy thông tin của sinh viên theo ID
+      public function read($id) {
+        foreach($this->students as $student) {
+          if($student->getId() == $id) {
+            return $student;
+          }
+        }
+        return null;
+      }
 
 
 }
