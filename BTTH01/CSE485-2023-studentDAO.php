d2e892a2aa8f707b57dcf3f22bee7b526a2e69b1
Datbui123 <datbui7102002@gmail.com>
2023-05-09 09:05:02 +0700

    them delete, getall

--- BTTH01/CSE485-2023-studentDAO.php
+++ BTTH01/CSE485-2023-studentDAO.php
@@ -1,7 +1,25 @@
 <?php
 class studentDAO extends Student{
     private $students = array();
-    
+    class StudentDAO {
+        private $students = array();
+        
+        public function delete($id) {
+          // Tìm kiếm sinh viên với id tương ứng trong cơ sở dữ liệu
+          // Xóa sinh viên khỏi mảng $students
+          foreach ($this->students as $key => $student) {
+            if ($student->getId() == $id) {
+              unset($this->students[$key]);
+              break;
+            }
+          }
+        }
+        
+        public function getAll() {
+          // Trả về tất cả sinh viên trong mảng $students
+          return $this->students;
+        }
+      }
 
 
 }
