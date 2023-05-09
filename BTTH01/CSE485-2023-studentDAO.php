cac9f7789ebf0f0511259425b6e7406d55c193b0
unknown <nguyenduyanh.tit@gmail.com>
2023-05-09 09:01:53 +0700

    asv

--- BTTH01/CSE485-2023-studentDAO.php
+++ BTTH01/CSE485-2023-studentDAO.php
@@ -1,8 +1,17 @@
 <?php
 class studentDAO extends Student{
     private $students = array();
-    
 
+    public function delete($id) {
+        // Tìm kiếm sinh viên với id tương ứng trong cơ sở dữ liệu
+        // Xóa sinh viên khỏi mảng $students
+        foreach ($this->students as $key => $student) {
+          if ($student->getId() == $id) {
+            unset($this->students[$key]);
+            break;
+          }
+        }
+      }
 
 }
 ?>
