bc9ddc4d9a5fb91a94adf2779294e5a718d546f7
unknown <nguyenduyanh.tit@gmail.com>
2023-05-09 09:04:45 +0700

    them update

--- BTTH01/CSE485-2023-studentDAO.php
+++ BTTH01/CSE485-2023-studentDAO.php
@@ -1,7 +1,16 @@
 <?php
 class studentDAO extends Student{
     private $students = array();
-    
+    public function update(Student $student) {
+        // Tìm kiếm sinh viên với id tương ứng trong cơ sở dữ liệu
+        // Cập nhật thông tin sinh viên
+        foreach ($this->students as $key => $s) {
+          if ($s->getId() == $student->getId()) {
+            $this->students[$key] = $student;
+            break;
+          }
+        }
+      }
 
 
 }
